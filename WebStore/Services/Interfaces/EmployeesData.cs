a6bb2fc8ba7041516630c8ba9bd937acd3b45f85
Palych18 <pavel_egoroff@mail.ru>
2021-06-01 16:01:20 +0300

    Интерфейс сервиса управления сотрудниками

--- WebStore/Services/Interfaces/EmployeesData.cs
+++ WebStore/Services/Interfaces/EmployeesData.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Models;
 
 namespace WebStore.Services.Interfaces
 {
-    public class EmployeesData
+    public interface EmployeesData
     {
+        IEnumerable<Employee> GetAll();
+
+        Employee Get(int id);
+
+        int Add(Employee employee);
+
+        void Update(Employee employee);
+
+        bool Delete(int id);
     }
 }

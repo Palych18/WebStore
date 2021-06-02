e0818f7862da00bc49ce2f724878468a2a4bc38a
Palych18 <pavel_egoroff@mail.ru>
2021-06-02 13:19:15 +0300

    Редактирование сотрудников

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Models;
 using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -23,10 +24,56 @@ namespace WebStore.Controllers
         {
             var employee = _EmployeesData.Get(id);
 
-            if (employee == null)
+            if (employee is null)
                 return NotFound();
 
             return View(employee);
         }
+
+        public IActionResult Create() => View();
+
+        public IActionResult Edit(int id)
+        {
+            var employee = _EmployeesData.Get(id);
+            if (employee is null) 
+                return NotFound();
+
+            var view_model = new EmployeeViewModel
+            {
+                Id = employee.Id,
+                SurName = employee.SurName,
+                Name = employee.Name,
+                Patronymic = employee.Patronymic,
+                Age = employee.Age,
+                WorkExperience = employee.WorkExperience,
+                Post = employee.Post,
+                Education = employee.Education,
+            };
+
+            return View(view_model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EmployeeViewModel Model)
+        {
+
+            var employee = new Employee
+            {
+                Id = Model.Id,
+                SurName = Model.SurName,
+                Name = Model.Name,
+                Patronymic = Model.Patronymic,
+                Age = Model.Age,
+                WorkExperience = Model.WorkExperience,
+                Post = Model.Post,
+                Education = Model.Education,
+            };
+
+            _EmployeesData.Update(employee);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id) => View();
     }
 }
---
+++ WebStore/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+
+        public string SurName { get; set; }
+
+        public string Name { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public int Age { get; set; }
+
+        public int WorkExperience { get; set; }
+
+        public string Post { get; set; }
+
+        public string Education { get; set; }
+    }
+}

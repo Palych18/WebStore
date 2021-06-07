c160f35c02ed328586c1119571b2be57f8c52029
Palych18 <pavel_egoroff@mail.ru>
2021-06-07 20:36:19 +0300

    Определение секции и бренда

--- WebStore.Domain/Entities/Base/Interfaces/IOrderedEntity.cs
+++ WebStore.Domain/Entities/Base/Interfaces/IOrderedEntity.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WebStore.Domain.Entities.Base.Interfaces
 {
-    public interface IOrderEntity : IEntity
+    public interface IOrderedEntity : IEntity
     {
         int Order { get; }
     }

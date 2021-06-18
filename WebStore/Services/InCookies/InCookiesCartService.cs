15ef88446fb5ef2cf44717cadd6124a10896f811
Palych18 <pavel_egoroff@mail.ru>
2021-06-18 20:28:16 +0300

    Логика управления корзиной

--- WebStore/Services/InCookies/InCookiesCartService.cs
+++ WebStore/Services/InCookies/InCookiesCartService.cs
@@ -17,6 +17,32 @@ namespace WebStore.Services.InCookies
         private readonly IHttpContextAccessor _HttpContextAccessor;
         private readonly IProductData _ProductData;
         private readonly string _CartName;
+        private Cart Cart
+        {
+            get
+            {
+                var context = _HttpContextAccessor.HttpContext;
+                var cookies = context!.Response.Cookies;
+
+                var cart_cookie = context.Request.Cookies[_CartName];
+                if (cart_cookie is null)
+                {
+                    var cart = new Cart();
+                    cookies.Append(_CartName, JsonConvert.SerializeObject(cart));
+                    return cart;
+                }
+
+                ReplaceCookies(cookies, cart_cookie);
+                return JsonConvert.DeserializeObject<Cart>(cart_cookie);
+            }
+            set => ReplaceCookies(_HttpContextAccessor.HttpContext!.Response.Cookies, JsonConvert.SerializeObject(value));
+        }
+
+        private void ReplaceCookies(IResponseCookies cookies, string cookie)
+        {
+            cookies.Delete(_CartName);
+            cookies.Append(_CartName, cookie);
+        }
         public InCookiesCartService(IHttpContextAccessor HttpContextAccessor, IProductData ProductData)
         {
             _HttpContextAccessor = HttpContextAccessor;
@@ -29,22 +55,56 @@ namespace WebStore.Services.InCookies
 
         public void Add(int id)
         {
+            var cart = Cart;
+
+            var item = cart.Items.FirstOrDefault(i => i.ProductId == id);
+            if (item is null)
+                cart.Items.Add(new CartItem { ProductId = id });
+            else
+                item.Quantity++;
+
+            Cart = cart;
         }
 
         public void Decrement(int id)
         {
+            var cart = Cart;
+
+            var item = cart.Items.FirstOrDefault(i => i.ProductId == id);
+            if (item is null) return;
+
+            if (item.Quantity > 0)
+                item.Quantity--;
+
+            if (item.Quantity <= 0)
+                cart.Items.Remove(item);
+
+            Cart = cart;
         }
 
         public void Remove(int id)
-        {            
+        {
+            var cart = Cart;
+
+            var item = cart.Items.FirstOrDefault(i => i.ProductId == id);
+            if (item is null) return;
+
+            cart.Items.Remove(item);
+
+            Cart = cart;
         }
 
         public void Clear()
-        {            
+        {
+            
+            var cart = Cart;
+            cart.Items.Clear();
+            Cart = cart;
         }
 
         public CartViewModel GetViewModel()
-        {            
+        {
+            throw new System.NotImplementedException();
         }
     }
 }

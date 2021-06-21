9bb8e19cb34871cf0119eec301c10c36c0368753
Palych18 <pavel_egoroff@mail.ru>
2021-06-21 10:14:15 +0300

    Заготовка области администратора

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -101,7 +101,11 @@ namespace WebStore
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
-            {    
+            {
+                endpoints.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");

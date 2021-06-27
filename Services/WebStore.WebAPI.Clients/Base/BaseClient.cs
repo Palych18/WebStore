c40e9d1bb24df6b6999a3022cf04d1ced0031757
Palych18 <pavel_egoroff@mail.ru>
2021-06-27 23:31:12 +0300

    Заготовка клиента

---
+++ Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.WebAPI.Clients.Base
+{
+    public abstract class BaseClient
+    {
+
+    }
+}

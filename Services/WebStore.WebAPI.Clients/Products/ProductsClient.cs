f1999bed2163b1d2dc7db828becdc16bcee4fab6
Palych18 <pavel_egoroff@mail.ru>
2021-07-02 16:33:57 +0300

    Доработка контроллера и клиента для использования в них DTO-моделей

--- Services/WebStore.WebAPI.Clients/Products/ProductsClient.cs
+++ Services/WebStore.WebAPI.Clients/Products/ProductsClient.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using WebSrore.Interfaces;
 using WebStore.Domain;
+using WebStore.Domain.DTO;
 using WebStore.Domain.Entities;
 using WebStore.Services.Interfaces;
 using WebStore.WebAPI.Clients.Base;
@@ -17,21 +18,21 @@ namespace WebStore.WebAPI.Clients.Products
     {
         public ProductsClient(HttpClient Client) : base(Client, WebAPIAddress.Products) { }
 
-        public IEnumerable<Section> GetSections() => Get<IEnumerable<Section>>($"{Address}/section");
+        public IEnumerable<Section> GetSections() => Get<IEnumerable<SectionDTO>>($"{Address}/section").FromDTO();
 
-        public Section GetSection(int id) => Get<Section>($"{Address}/sections/{id}");
+        public Section GetSection(int id) => Get<SectionDTO>($"{Address}/sections/{id}").FromDTO();
 
-        public IEnumerable<Brand> GetBrands() => Get<IEnumerable<Brand>>($"{Address}/brand");
+        public IEnumerable<Brand> GetBrands() => Get<IEnumerable<BrandDTO>>($"{Address}/brand").FromDTO();
 
-        public Brand GetBrand(int id) => Get<Brand>($"{Address}/brands/{id}");
+        public Brand GetBrand(int id) => Get<BrandDTO>($"{Address}/brands/{id}").FromDTO();
 
         public IEnumerable<Product> GetProducts(ProductFilter Filter = null)
         {
             var response = Post(Address, Filter ?? new ProductFilter());
-            var products = response.Content.ReadFromJsonAsync<IEnumerable<Product>>().Result;
-            return products;
+            var products = response.Content.ReadFromJsonAsync<IEnumerable<ProductDTO>>().Result;
+            return products.FromDTO();
         }
 
-        public Product GetProductById(int Id) => Get<Product>($"{Address}/{Id}");
+        public Product GetProductById(int Id) => Get<ProductDTO>($"{Address}/{Id}").FromDTO();
     }
 }

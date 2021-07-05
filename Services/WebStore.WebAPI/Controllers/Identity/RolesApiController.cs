7a1f6d9d9df30ae69973a9a2af468b53acda1fac
Palych18 <pavel_egoroff@mail.ru>
2021-07-05 09:57:05 +0300

    Заготовка контроллеров для работы с пользователями и ролями через WebAPI

---
+++ Services/WebStore.WebAPI/Controllers/Identity/RolesApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSrore.Interfaces;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.WebAPI.Controllers.Identity
+{
+    [Route(WebAPIAddress.Identity.Roles)]
+    [ApiController]
+    public class RolesApiController : ControllerBase
+    {
+        private readonly RoleStore<Role> _RoleStore;
+
+        public RolesApiController(WebStoreDB db)
+        {
+            _RoleStore = new RoleStore<Role>(db);
+        }
+    }
+}
---
+++ Services/WebStore.WebAPI/Controllers/Identity/UsersApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSrore.Interfaces;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.WebAPI.Controllers.Identity
+{
+    [Route(WebAPIAddress.Identity.Users)]
+    [ApiController]
+    public class UsersApiController : ControllerBase
+    {
+        private readonly UserStore<User, Role, WebStoreDB> _UserStore;
+
+        public UsersApiController(WebStoreDB db)
+        {
+            _UserStore = new UserStore<User, Role, WebStoreDB>(db);
+        }
+    }
+}

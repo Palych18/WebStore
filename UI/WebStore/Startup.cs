0a9495c0a764344e92ace3fcc6956c39c0d77801
Palych18 <pavel_egoroff@mail.ru>
2021-07-05 14:31:27 +0300

    Удаление хлама, проверка инициализации.

--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -38,27 +38,8 @@ namespace WebStore
         }
 
         public void ConfigureServices(IServiceCollection services)
-        {
-            //var database_name = Configuration["Database"];
-
-            //switch (database_name)
-            //{
-            //    case "MSSQL":
-            //        services.AddDbContext<WebStoreDB>(opt => opt.UseSqlServer(Configuration.GetConnectionString("MSSQL")));
-            //        break;
-
-            //    case "Sqlite":
-            //        services.AddDbContext<WebStoreDB>(opt =>
-            //            opt.UseSqlite(
-            //                Configuration.GetConnectionString("Sqlite"),
-            //                o => o.MigrationsAssembly("WebStore.DAL.Sqlite")));
-            //        break;
-            //}
-
-            //services.AddTransient<WebStoreDBInitializer>();
-
-            services.AddIdentity<User, Role>()
-                //.AddEntityFrameworkStores<WebStoreDB>()
+        {            
+            services.AddIdentity<User, Role>()                
                 .AddDefaultTokenProviders();
 
             services.AddHttpClient("WebStoreAPIIdentity", client => client.BaseAddress = new Uri(Configuration["WebAPI"]))
@@ -117,11 +98,8 @@ namespace WebStore
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env/*, IServiceProvider services*/)
-        {
-            //using (var scope = services.CreateScope())
-            //    scope.ServiceProvider.GetRequiredService<WebStoreDBInitializer>().Initialize();
-
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();

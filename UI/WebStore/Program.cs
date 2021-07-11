147a89bce7ce2d1b4e329bf62362a485059c5688
Palych18 <pavel_egoroff@mail.ru>
2021-07-11 09:35:59 +0300

    Настройка системы логирования в коде

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,13 @@ namespace WebStore
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                //.ConfigureLogging((host, log) => log
+                //    .ClearProviders()
+                //    .AddDebug()
+                //    //.AddConsole(opt => opt.IncludeScopes = true)
+                //    .AddEventLog()
+                //    .AddFilter<ConsoleLoggerProvider>("Microsoft", LogLevel.Information)
+                //    .AddFilter<ConsoleLoggerProvider>("Microsoft", Level => Level >= LogLevel.Information))
                 .ConfigureWebHostDefaults(host => host
                    .UseStartup<Startup>()
                 )

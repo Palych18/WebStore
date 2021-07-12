b74912630cc2bbebb2c8a431a71d0b7352f624cd
Palych18 <pavel_egoroff@mail.ru>
2021-07-12 21:34:22 +0300

    Подготовка к тестированию методов

---
+++ Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Areas.Admin.Controllers;
+using WebStore.Domain;
+using WebStore.Domain.Entities;
+using WebStore.Services.Interfaces;
+using Assert = Xunit.Assert;
+
+namespace WebStore.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTests
+    {        
+        [TestMethod]
+        public void Index_Returns_View()
+        {
+            var configuration_mock = new Mock<IConfiguration>();
+            var product_data_mock = new Mock<IProductData>();
+            product_data_mock.Setup(s => s.GetProducts(It.IsAny<ProductFilter>()))
+               .Returns(Enumerable.Empty<Product>());
+            
+            var controller = new HomeController(configuration_mock.Object);
+
+            var result = controller.Index(product_data_mock.Object);
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}

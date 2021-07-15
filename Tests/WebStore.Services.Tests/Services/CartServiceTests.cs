9d3708e3f83640e3cfa9e3a984111d6e835a50e7
Palych18 <pavel_egoroff@mail.ru>
2021-07-15 18:57:10 +0300

    Подготовка к тестированию сервиса корзины - тетирование моделей

---
+++ Tests/WebStore.Services.Tests/Services/CartServiceTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities;
+using WebStore.ViewModels;
+using Assert = Xunit.Assert;
+
+namespace WebStore.Services.Tests.Services
+{
+    [TestClass]
+    public class CartServiceTests
+    {
+        private Cart _Cart;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _Cart = new Cart
+            {
+                Items = new List<CartItem>
+                {
+                    new() {ProductId = 1, Quantity = 1},
+                    new() {ProductId = 2, Quantity = 3},
+                }
+            };
+        }
+
+        [TestMethod]
+        public void Cart_Class_ItemsCount_returns_Correct_Quantity()
+        {
+            var cart = _Cart;
+            var expected_items_count = _Cart.Items.Sum(i => i.Quantity);
+
+            var actual_items_count = cart.ItemsCount;
+
+            Assert.Equal(expected_items_count, actual_items_count);
+        }
+
+        [TestMethod]
+        public void CartViewModel_Returns_Correct_ItemsCount()
+        {
+            var cart_view_model = new CartViewModel
+            {
+                Items = new[]
+                {
+                    ( new ProductViewModel { Id = 1, Name = "Product 1", Price = 0.5m }, 1 ),
+                    ( new ProductViewModel { Id = 2, Name = "Product 2", Price = 1.5m }, 3 ),
+                }
+            };
+            const int expected_count = 4;
+
+            var actual_count = cart_view_model.ItemsCount;
+
+            Assert.Equal(expected_count, actual_count);
+        }
+    }
+}

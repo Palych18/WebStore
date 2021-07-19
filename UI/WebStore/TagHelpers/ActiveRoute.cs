16eb28a06ccd4a8873bbf2cf426b41221d1cc609
Palych18 <pavel_egoroff@mail.ru>
2021-07-19 21:21:28 +0300

    Подготовка инфраструктуры таг-хэлпера

--- UI/WebStore/TagHelpers/ActiveRoute.cs
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +13,37 @@ namespace WebStore.TagHelpers
     {
         private const string AttributeName = "ws-is-active-route";
 
+        [HtmlAttributeName("asp-controller")]
+        public string Controller { get; set; }
+
+        [HtmlAttributeName("asp-action")]
+        public string Action { get; set; }
+
+        [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-all-route-")]
+        public Dictionary<string, string> RouteValues { get; set; } = new(StringComparer.OrdinalIgnoreCase);
+
+        [ViewContext, HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            
+            if (IsActive())
+                MakeActive(output);
+
+
+            output.Attributes.RemoveAll(AttributeName);
+        }
+
+        private bool IsActive()
+        {
+
+
+            return true;
+        }
+
+        private void MakeActive(TagHelperOutput output)
+        {
+
         }
     }
 }

929d96a9666eecd84f89b328428873d01157560c
Palych18 <pavel_egoroff@mail.ru>
2021-07-20 16:34:05 +0300

    Выделение активной секции

---
+++ UI/WebStore/ViewModels/SelectableSectionsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace WebStore.ViewModels
+{
+    public class SelectableSectionsViewModel
+    {
+        public IEnumerable<SectionViewModel> Sections { get; init; }
+
+        public int? SectionId { get; init; }
+
+        public int? ParentSectionId { get; init; }
+    }
+}

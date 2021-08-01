398feb84e72e8427b072a8ab8f803d71ddd2c42d
Palych18 <pavel_egoroff@mail.ru>
2021-08-01 19:13:13 +0300

    Подготовка контроллера AJAX

---
+++ UI/WebStore/Controllers/AjaxTestController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.ViewModels;
+
+namespace WebStore.Controllers
+{
+    public class AjaxTestController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public async Task<IActionResult> GetJSON(int? id, string msg, int Delay = 2000)
+        {
+            await Task.Delay(Delay);
+
+            return Json(new 
+            {
+                Message = $"Response (id:{id ?? 1}): {msg ?? "<null>"}",
+                ServerTime = DateTime.Now
+            });
+        }
+
+        public async Task<IActionResult> GetHTML(int? id, string msg, int Delay = 2000)
+        {
+            await Task.Delay(Delay);
+
+            return PartialView("Partial/_DataView", new AjaxTestDataViewModel
+            {
+                Id = id ?? 1,
+                Message = msg
+            });
+        }
+    }
+}
